206f04eac7458f76a7ac6ae9080e99ba1f40fb1b
Giorgi Ghviniashvili <mr.g.ghv@gmail.com>
2024-06-03 14:41:46 +0400

    Update hierarchy.js

--- hierarchy.js
+++ hierarchy.js
@@ -1,26 +1,79 @@
-import { groups, timeFormat } from 'd3'
+import { groups } from 'd3'
+
+// GroupBy: {
+// 	value: 'AcademicScore',
+// 	label: 'Academic score',
+// 	groupFn: value => {
+// 		const group = academicScoreGroupFn(value)
+
+// 		if (Array.isArray(group)) {
+// 			return group.join(" - ")
+// 		}
+		
+// 		return group
+// 	}
+// }
 
 // Returns { key: 'root', children: [ ... ] }
-export const getFlare = (flatdata, groupBy, keyFn) => {
-	const clusterify = (obj, children) => {
-		if (Array.isArray(children[0])) {
-			obj.children = children.map(([key, arr]) =>
-				clusterify({ key, id: `${key}_${getRandomId()}` }, arr)
-			)
-		} else {
-			obj.children = children.map(d => {
-				const obj = { ...d, originalData: d }
+export const getFlare = (
+	flatdata,
+	groupByOptions,
+	leafNodeKey,
+	rootNode = { key: 'root', id: 'root' }
+) => {
+	const getNode = d => {
+		const newNode = { ...d }
 
-				if (!obj.key && keyFn) {
-					obj.key = keyFn(d)
-				}
-				return obj
-			})
+		if (!newNode.key) {
+			const key = leafNodeKey ? d[leafNodeKey] : getRandomId()
+			newNode.id = `${key}_${getRandomId()}`
+			newNode.key = key
 		}
-		return obj
+
+		return newNode
 	}
 
-	const grouped = groups(flatdata, ...groupBy.map(key => d => d[key]))
+	// Recursive
+	const clusterify = (node, children, level) => {
+		if (!node.children && children.length) {
+			node.children = []
+		}
 
-	return clusterify({ key: 'root' }, grouped)
+		if (Array.isArray(children[0])) {
+			children.forEach(([key, arr]) => {
+				if (!key) {
+					clusterify(node, arr, level + 1)
+					return
+				}
+
+				const newNode = {
+					key,
+					id: `${key}_${getRandomId()}`,
+					groupedBy: groupBy[level]
+				}
+				const clustered = clusterify(newNode, arr, level + 1)
+				node.children.push(clustered)
+			})
+		} else {
+			children.forEach(d => {
+				const newNode = getNode(d)
+				newNode.groupedBy = groupBy[level] || leafNodeKey
+				node.children.push(newNode)
+			})
+		}
+
+		return node
+	}
+
+	const grouped = groups(
+		flatdata,
+		...groupByOptions.map(({ value: key, groupFn }) => {
+			if (groupFn) {
+				return d => groupFn(d[key])
+			}
+			return d => d[key]
+		})
+	)
+
+	return clusterify(rootNode, grouped, 0)
 }
